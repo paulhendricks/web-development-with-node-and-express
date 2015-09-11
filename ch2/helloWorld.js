10e771f1148c19199df0fcb81ff14082234a0c53
paulhendricks <paul.hendricks.2013@owu.edu>
2015-09-11 11:00:38 -0400

    writing my first web server

---
+++ ch2/helloWorld.js
@@ -0,0 +1,11 @@
+/**
+ * Created by phendricks on 9/11/2015.
+ */
+var http = require("http")
+
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Hello world!");
+}).listen(3000);
+
+console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
